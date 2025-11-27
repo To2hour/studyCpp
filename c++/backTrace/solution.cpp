64634cbe25d419709ebb545cbf8e3823eca88194
2hour <2904260265@example.com>
2025-11-27 10:11:45 +0800

    递增子序列决策树写法

--- c++/backTrace/solution.cpp
+++ c++/backTrace/solution.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <climits>
 #include <cstddef>
 #include <iostream>
 #include <math.h>
@@ -101,5 +102,86 @@ public:
       right--;
     }
     return true;
+  }
+};
+
+// 78. 子集
+class Solution4 {
+public:
+  vector<int> path;
+  vector<vector<int>> res;
+  vector<vector<int>> subsets(vector<int> &nums) {
+    auto dfs = [&](auto &&dfs, int index) -> void {
+      res.push_back(path);
+      if (index == nums.size()) {
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        path.push_back(nums[i]);
+        dfs(dfs, i + 1);
+        path.pop_back();
+      }
+    };
+    dfs(dfs, 0);
+    return res;
+  }
+};
+
+class Solution5 {
+public:
+  vector<int> path;
+
+  vector<vector<int>> res;
+  vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+    vector<bool> used = vector<bool>(nums.size(), false);
+    sort(nums.begin(), nums.end());
+    auto dfs = [&](auto &&dfs, int index) -> void {
+      res.push_back(path);
+      if (index == nums.size()) {
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        if (i > 0 && nums[i] == nums[i - 1] && !used[i]) {
+          continue;
+        }
+        path.push_back(nums[i]);
+        dfs(dfs, i + 1);
+        path.pop_back();
+      }
+    };
+    dfs(dfs, 0);
+    return res;
+  }
+};
+//递增子序列
+class Solution6 {
+public:
+  vector<vector<int>> res;
+  vector<int> path;
+  vector<vector<int>> findSubsequences(vector<int> &nums) {
+    auto dfs = [&](auto &&dfs, int pre, int startIndex) -> void {
+      if (startIndex == nums.size()) {
+        if (path.size() >= 2) {
+          res.push_back(path);
+        }
+        return;
+      }
+     //不选
+     //对于两个相同的值，比如223。如果第一个2不选，那可以选择第二个2,可以出现23。
+     //而如果选择了第一个2，那第二个2必须选，否则会重复出现。
+      if(pre != nums[startIndex]){
+        dfs(dfs, pre, startIndex + 1);
+      }
+      //选
+      //这里如果不加条件，会导致path中出现不对的数据。
+      if (pre <= nums[startIndex]) {
+        path.push_back(nums[startIndex]);
+        dfs(dfs, nums[startIndex], startIndex + 1);
+        path.pop_back();
+      }
+       
+    };
+    dfs(dfs, INT_MIN, 0);
+    return res;
   }
 };
